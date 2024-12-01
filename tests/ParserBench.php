d6e1a8ab26c8ae699ec4dbe88fd5946efb10d782
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2024-12-01 20:31:54 +0100

    restore bench tests

---
+++ tests/ParserBench.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bakame\Http\StructuredFields;
+
+use PhpBench\Attributes as Bench;
+
+final class ParserBench
+{
+    private readonly Parser $parser;
+
+    public function __construct()
+    {
+        $this->parser = Parser::new();
+    }
+
+    #[Bench\Iterations(4)]
+    #[Bench\OutputTimeUnit('seconds')]
+    #[Bench\Assert('mode(variant.mem.peak) < 2097152'), Bench\Assert('mode(variant.time.avg) < 10000000')]
+    public function benchParsingAList(): void
+    {
+        $httpValue = '("lang" "en-US"); expires=@1623233894; samesite=Strict; secure';
+        for ($i = 0; $i < 100_000; $i++) {
+            $this->parser->parseList($httpValue);
+        }
+    }
+
+    #[Bench\Iterations(4)]
+    #[Bench\OutputTimeUnit('seconds')]
+    #[Bench\Assert('mode(variant.mem.peak) < 2097152'), Bench\Assert('mode(variant.time.avg) < 10000000')]
+    public function benchParsingAnItem(): void
+    {
+        $httpValue = '"lang"; expires=@1623233894; samesite=Strict; secure';
+        for ($i = 0; $i < 100_000; $i++) {
+            $this->parser->parseItem($httpValue);
+        }
+    }
+
+    #[Bench\Iterations(4)]
+    #[Bench\OutputTimeUnit('seconds')]
+    #[Bench\Assert('mode(variant.mem.peak) < 2097152'), Bench\Assert('mode(variant.time.avg) < 10000000')]
+    public function benchParsingAnDictionary(): void
+    {
+        $httpValue = 'lang="en-US"; samesite=Strict; secure, type=42.0; expires=@1623233894';
+        for ($i = 0; $i < 100_000; $i++) {
+            $this->parser->parseDictionary($httpValue);
+        }
+    }
+}
